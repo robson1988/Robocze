e171dbc084cfd53c8edf448dedd72196e2813e51
RBcode <40271285+robson1988@users.noreply.github.com>
2020-01-16 11:13:42 +0100

    Add files via upload

---
+++ multi_language_sitemap_url_reader.php
@@ -0,0 +1,49 @@
+<?php
+
+$data = array_map('str_getcsv', file('Linki.csv', FILE_SKIP_EMPTY_LINES));
+
+$doc = new DOMDocument('1.0');
+$doc->formatOutput=true;
+
+$root = $doc->createElement('urlset');
+$doc->appendChild($root); 
+
+foreach($data as $z => $link) {
+
+foreach ($link as $y => $value) {
+	if($y == 5) { break;} else {
+		
+	if( !isset($link[$z]) ) $link[$z] = '' ;
+	
+	
+    $url = $doc->createElement('url');
+    $root->appendChild($url);
+
+    $loc = $doc->createElement('loc',$link[$y]);
+    $url->appendChild($loc);
+	
+	$xmlink = $doc->createElement('xhtml:link','rel="canonical" href="'.$link[$y].'"/>;');
+	$url->appendChild($xmlink);
+	$xmlink = $doc->createElement('xhtml:link','rel="alternate" hreflang="pl" href="'.$link[0].'"/>;');
+	$url->appendChild($xmlink);
+	$xmlink = $doc->createElement('xhtml:link','rel="alternate" hreflang="en" href="'.$link[1].'"/>;');
+	$url->appendChild($xmlink);
+	$xmlink = $doc->createElement('xhtml:link','rel="alternate" hreflang="de" href="'.$link[2].'"/>;');
+	$url->appendChild($xmlink);
+	$xmlink = $doc->createElement('xhtml:link','rel="alternate" hreflang="it" href="'.$link[3].'"/>;');
+	$url->appendChild($xmlink);
+	$xmlink = $doc->createElement('xhtml:link','rel="alternate" hreflang="ru" href="'.$link[4].'"/>;');
+	$url->appendChild($xmlink);
+	
+    $changefreq = $doc->createElement('changefreq','daily');
+    $url->appendChild($changefreq);
+
+    $priority = $doc->createElement('priority',$link[5]);
+    $url->appendChild($priority);
+
+	}
+  }
+}
+
+  //Save XML as a file
+  echo $doc->save('sitemap.xml');
